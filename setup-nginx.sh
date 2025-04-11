86d41244a2aa2386cc7f99701f83867e11aeed45
ECClabsdemos <94687705+ECClabs@users.noreply.github.com>
2025-04-11 15:02:52 +0530

    Update setup-nginx.sh

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -8,4 +8,4 @@ apt -y install nginx
 rm /var/www/html/index.nginx-debian.html
 
 # Create the default page for the region
-wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-distribute-load-with-traffic-manager/master/index.$1.html -O /var/www/html/index.html
+wget https://raw.githubusercontent.com/ECClabs/mslearn-distribute-load-with-traffic-manager/master/index.$1.html -O /var/www/html/index.html
